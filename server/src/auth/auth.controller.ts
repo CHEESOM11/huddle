81c4f750be9084757b873058fc288e4f37176d32
Ofulue Chisom <ofuluechisom10@gmail.com>
2026-09-09 02:38:02 +0100

    Migration from express to NestJS

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Post,
+} from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.signUp(name, email, password);
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.login(email, password);
+  }
+
+  @Post('forgot-password')
+  async forgotPassword(@Body('email') email: string) {
+    return this.authService.forgotPassword(email);
+  }
+
+  @Post('reset-password')
+  async resetPassword(
+    @Body('password') password: string,
+    @Headers('authorization') authorization: string,
+  ) {
+    const accessToken = authorization?.replace('Bearer ', '');
+
+    return this.authService.resetPassword(password, accessToken);
+  }
+
+  @Get('google')
+  async googleLogin() {
+    return this.authService.googleLogin();
+  }
+}
---
+++ server/src/config/supabase.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.supabase = void 0;
+const supabase_js_1 = require("@supabase/supabase-js");
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_PUBLISHABLE_KEY;
+exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey, {
+    auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+        detectSessionInUrl: false,
+    },
+});
+//# sourceMappingURL=supabase.js.map
