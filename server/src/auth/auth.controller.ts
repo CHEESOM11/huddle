5be04444b7aff113b3e5337535dd9d079fedd1d4
Ofulue Chisom <ofuluechisom10@gmail.com>
2026-09-11 00:28:47 +0100

    Add channel invite endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,70 +1,3 @@
-// import {
-//   Body,
-//   Controller,
-//   Get,
-//   Headers,
-//   Post,
-// } from '@nestjs/common';
-
-// import { AuthService } from './auth.service';
-
-// @Controller('auth')
-// export class AuthController {
-//   constructor(private readonly authService: AuthService) {}
-
-//   @Post('register')
-//   async signUp(
-//     @Body('name') name: string,
-//     @Body('email') email: string,
-//     @Body('password') password: string,
-//   ) {
-//     return this.authService.signUp(name, email, password);
-//   }
-
-//   @Post('login')
-//   async login(
-//     @Body('email') email: string,
-//     @Body('password') password: string,
-//   ) {
-//     return this.authService.login(email, password);
-//   }
-
-//   @Get('me')
-//   async getCurrentUser(@Headers('authorization') authorization: string) {
-//     const accessToken = authorization?.replace('Bearer ', '');
-
-//     return this.authService.getCurrentUser(accessToken);
-//   }
-
-//   @Post('forgot-password')
-//   async forgotPassword(@Body('email') email: string) {
-//     return this.authService.forgotPassword(email);
-//   }
-
-//   @Post('reset-password')
-//   async resetPassword(
-//     @Body('password') password: string,
-//     @Headers('authorization') authorization: string,
-//   ) {
-//     const accessToken = authorization?.replace('Bearer ', '');
-
-//     return this.authService.resetPassword(password, accessToken);
-//   }
-
-//   @Get('me')
-//   async currentUser(@Headers('authorization') authorization: string) {
-//     const accessToken = authorization?.replace('Bearer ', '');
-
-//     return this.authService.getCurrentUser(accessToken);
-//   }
-
-//   @Get('google')
-//   async googleLogin() {
-//     return this.authService.googleLogin();
-//   }
-// }
-
-
 import {
   Body,
   Controller,
@@ -97,13 +30,8 @@ export class AuthController {
   }
 
   @Get('me')
-  async getCurrentUser(
-    @Headers('authorization') authorization: string,
-  ) {
-    const accessToken = authorization?.replace(
-      /^Bearer\s+/i,
-      '',
-    );
+  async getCurrentUser(@Headers('authorization') authorization: string) {
+    const accessToken = authorization?.replace(/^Bearer\s+/i, '');
 
     return this.authService.getCurrentUser(accessToken);
   }
@@ -118,16 +46,11 @@ export class AuthController {
     @Body('password') password: string,
     @Headers('authorization') authorization: string,
   ) {
-    const accessToken = authorization?.replace(
-      /^Bearer\s+/i,
-      '',
-    );
+    const accessToken = authorization?.replace('Bearer ', '');
 
-    return this.authService.resetPassword(
-      password,
-      accessToken,
-    );
+    return this.authService.resetPassword(password, accessToken);
   }
+
 
   @Get('google')
   async googleLogin() {
