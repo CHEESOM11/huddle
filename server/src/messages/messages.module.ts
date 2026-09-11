77ef0bddb8279b29314a29600f9f51931eeae001
Ofulue Chisom <ofuluechisom10@gmail.com>
2026-09-11 01:29:11 +0100

    Naming typo and comments corrected

--- server/src/messages/messages.module.ts
+++ server/src/messages/messages.module.ts
@@ -1,14 +1,3 @@
-// import { Module } from '@nestjs/common';
-
-// import { MessagesController } from './messages.controller';
-// import { MessagesService } from './messages.service';
-
-// @Module({
-//   controllers: [MessagesController],
-//   providers: [MessagesService],
-// })
-// export class MessagesModule {}
-
 import { Module } from '@nestjs/common';
 
 import { MessagesController } from './messages.controller';
