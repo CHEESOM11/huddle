c076dc12be826ace440d1462f33184cc8d5bc270
Ofulue Chisom <ofuluechisom10@gmail.com>
2026-09-12 18:40:10 +0100

    Configure CORS with dynamic client URL

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+  });
   app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 4000);
